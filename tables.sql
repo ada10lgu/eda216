8b9a3b7b744aae6beafff948bafb83cd94c16486
ada10lgu <ada10lgu@student.lth.se>
2014-02-10 20:50:33 +0100

    Snyggade till kod

--- tables.sql
+++ tables.sql
@@ -39,20 +39,20 @@ CREATE TABLE reservation (
 );
 
 
-insert into user(username,name,telephone) values ("pj","per",90000); 
-insert into user(username,name,telephone,address) values("mezz","lars",112,"hemma");
-insert into user(username,telephone,name,address) values("per", 044123456,"pher holm", "malmö");
+INSERT INTO user(username,name,telephone) VALUES ("pj","per",90000); 
+INSERT INTO user(username,name,telephone,address) VALUES ("mezz","lars",112,"hemma");
+INSERT INTO user(username,telephone,name,address) VALUES ("per", 044123456,"pher holm", "malmö");
 
-insert into theatre (name, seats) values ("SF-lund",9000);
-insert into theatre (name, seats) values ("SF-malmö",2);
-insert into theatre (name, seats) values ("Filmstaden-malmö",12);
+INSERT INTO theatre (name, seats) VALUES ("SF-lund",9000);
+INSERT INTO theatre (name, seats) VALUES ("SF-malmö",2);
+INSERT INTO theatre (name, seats) VALUES ("Filmstaden-malmö",12);
 
-insert into venue (date, movie, theatre) values ("2014-02-03","Night of the Day of the Dawn of the Son of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Hellbound, Flesh-Eating Subhumanoid Zombified Living Dead, Part 3 (2005)","SF-lund");
-insert into venue (date,movie,theatre) values ("2013-03-13","U die!? 2 the raccoon","SF-lund");
-insert into venue (date,movie,theatre) values ("2014-02-01","Movie","SF-Lund");
-insert into venue (date,movie,theatre) values ("2014-02-01","Movie","SF-Malmö");
-insert into venue (date,movie,theatre) values ("2014-02-03","Movie","SF-Malmö");
-insert into venue (date,movie,theatre) values ("2014-02-02","Movie","SF-Lund");
+INSERT INTO venue (date, movie, theatre) VALUES ("2014-02-03","Night of the Day of the Dawn of the Son of the Bride of the Return of the Revenge of the Terror of the Attack of the Evil, Mutant, Hellbound, Flesh-Eating Subhumanoid Zombified Living Dead, Part 3 (2005)","SF-lund");
+INSERT INTO venue (date,movie,theatre) VALUES ("2013-03-13","U die!? 2 the raccoon","SF-lund");
+INSERT INTO venue (date,movie,theatre) VALUES ("2014-02-01","Movie","SF-Lund");
+INSERT INTO venue (date,movie,theatre) VALUES ("2014-02-01","Movie","SF-Malmö");
+INSERT INTO venue (date,movie,theatre) VALUES ("2014-02-03","Movie","SF-Malmö");
+INSERT INTO venue (date,movie,theatre) VALUES ("2014-02-02","Movie","SF-Lund");
 
 
 # LIST ALL MOVIES THAT ARE SHOWN
@@ -64,7 +64,10 @@ SELECT date from venue where movie="Movie";
 SELECT date from venue where movie="U die!? 2 the raccoon";
 
 # LIST DATA ABOUT ALL VENUES (ignore long name)
-select * from venue as v left join theatre as t on v.theatre = t.name where movie not like "N%";
+SELECT * 
+FROM venue AS v 
+LEFT JOIN theatre AS t ON v.theatre = t.name 
+WHERE movie NOT LIKE "N%";
 
 
 # CREATE RESARVATION
